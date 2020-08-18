6801678b49b0af3086f5a62b441beeff0403e8d3
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-18 10:31:05 -0400

    removed unncessary comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Article from './Article.js';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
